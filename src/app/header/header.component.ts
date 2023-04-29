6e0e717f9206f875d0ccfc708635232b13aea4e7
petrzamecnik <petazamecek@gmail.com>
2023-04-29 11:16:53 +0200

    header update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -35,4 +35,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this._destroy$.next();
     this._destroy$.complete();
   }
+
+  navigateToHomepage() {
+    this._router.navigate(['/']);
+  }
 }
